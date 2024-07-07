0d5597179d10e30a2414cff751fba35203e28798
kiom_ <moki4994@gmail.com>
2024-07-07 23:06:42 +0530

    Merged separately kept upload component within upload page itself & changed the style for the uploaded file list

--- src/Components/Assignment/UploadPage.jsx
+++ src/Components/Assignment/UploadPage.jsx
@@ -1,34 +1,69 @@
-import { Button } from 'antd';
+import React from 'react';
 import {
   FileOutlined,
+  InboxOutlined,
 } from '@ant-design/icons';
-import UploadComponent from './UploadComponent';
 import { useNavigate } from 'react-router-dom';
+import { Button, Input, message, Upload } from 'antd';
+const { TextArea } = Input;
+const { Dragger } = Upload;
+
+const Uploadprops = {
+  name: 'file',
+  multiple: true,
+  action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log('Dropped files', e.dataTransfer.files);
+  },
+};
+
+var assignmentDetails = {
+  title:"Assignment 1 : Resume website",
+  description:"Build your resume as a website using HTML and CSS"
+}
+
 const UploadPage = () => {
   const navigate = useNavigate();
+  function AssignmentSubmit(){
+    var uploadCommentCheck = document.getElementById('UploadComments');
+    if(uploadCommentCheck.textContent!=='' && uploadCommentCheck.textContent!==' '){ // Can use regex for more control
+      console.log(uploadCommentCheck.textContent)
+      navigate('/assignments/upload/success');
+    }
+    else{
+      uploadCommentCheck.className = uploadCommentCheck.className + " ant-input-status-error"
+    }
+  }
     return (
       <div>
-        <Button type='primary'><FileOutlined/> Assignment 1 : Resume website</Button>
-        <div
-          style={{
-            backgroundColor:'white',
-            width:'60%',
-            // padding:'0.2%',
-            paddingTop:'0.1%',
-            paddingLeft:'2%',
-            paddingBottom:'0.5%',
-            marginTop:'1%',
-            }}
-        >
-      <p><b>Description:</b> <br></br>
-      <span style={{display:'inline-block', width: '20px'}}></span>
-      Re-create your resume as a website using HTML and CSS</p>
-        </div>
-          <div 
-          style={{
-            margin:'5%',
-          }}>
-          <UploadComponent/>
+        <Button type='primary' id='AssignmentTitleButton'><FileOutlined/> {assignmentDetails.title} </Button>
+        <div id='AssignementDescription'><b>Description:</b> {assignmentDetails.description}</div>
+          <div style={{margin:'3%'}}>
+            
+            <Dragger {...Uploadprops}> {/* Upload component - Start */}
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">Click or drag file to this area to upload</p>
+              <p className="ant-upload-hint">
+                Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+                banned files.
+              </p>
+            </Dragger> {/* Upload component - End */}
+            
+            <br/><br/>
+            <TextArea rows={4} placeholder="Comments" id='UploadComments'/>
           </div>
           <div
             style={{
@@ -37,7 +72,7 @@ const UploadPage = () => {
               margin:'2%',
             }}
           >
-            <Button type='primary' onClick={()=>navigate('/assignments/upload/success')}>Submit</Button>
+            <Button type='primary' onClick={AssignmentSubmit}>Submit</Button>
             <span style={{display:'inline-block', width: '3%'}}></span>
             <Button onClick={()=>navigate('/assignments')}>Cancel</Button>
           </div>
